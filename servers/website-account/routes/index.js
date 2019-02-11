d10d6aee70a3afb944ec9af14474d0621706881f
wanglei <wanglei@yunify.com>
2019-02-11 22:24:45 +0800

    refactor: Refactor template title

--- servers/website-account/routes/index.js
+++ servers/website-account/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get(/\/.*/, function(req, res, next) {
-  res.render('index', { title: 'Blog' });
+  res.render('index', { title: 'Website Account' });
 });
 
 module.exports = router;
--- servers/website-admin/routes/componentsRouter.js
+++ servers/website-admin/routes/componentsRouter.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('components', { title: 'Internal Components' });
+  res.render('components', { title: '_Internal Components' });
 });
 
 module.exports = router;
