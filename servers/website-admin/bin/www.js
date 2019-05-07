47e7ad2459539e4af603069a0930b6d5a30af361
wanglei <wanglei@yunify.com>
2019-05-07 22:25:05 +0800

    refactor: Refactor server config

--- servers/website-admin/bin/www.js
+++ servers/website-admin/bin/www.js
@@ -8,7 +8,7 @@ var notice = require('<common>/notice');
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || '3001');
 app.set('port', port);
 
 /**
