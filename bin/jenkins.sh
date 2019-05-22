76ef5b8babd09e9c5ab509cb2459f0b3d29a0c83
wanglei <wanglei@yunify.com>
2019-05-22 12:38:42 +0800

    feature: Add helmet and redis session

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -3,6 +3,9 @@
 cp ${servers_config} servers/common/config.json
 cp ${browsers_config} browsers/common/config.json
 
+ln -sf /var/lib/jenkins/workspace/website/servers/common /var/lib/jenkins/workspace/website/servers/website-account/common
+ln -sf /var/lib/jenkins/workspace/website/servers/common /var/lib/jenkins/workspace/website/servers/website-admin/common
+
 cd browsers
 npm install
 npm run build
--- servers/website-account/app.js
+++ servers/website-account/app.js
@@ -1,24 +1,46 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
+const Koa = require('koa');
+const bodyparser = require('koa-bodyparser');
+const helmet = require("koa-helmet");
+const json = require('koa-json');
+const logger = require('koa-logger');
+const onerror = require('koa-onerror');
 const path = require('path');
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
+const redisStore = require('koa-redis');
+const session = require('koa-generic-session');
+const views = require('koa-views');
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const CONFIG = require('./common/config');
+
+
+const ACCOUNT_CONFIG = CONFIG.ACCOUNT;
+
+const app = new Koa();
+app.keys = ['keys', 'keykeys'];
 
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
+app.use(helmet({
+  dnsPrefetchControl: false,
+}));
+
+app.use(session({
+  store: redisStore({
+    host: CONFIG.REDIS_HOST,
+    port: CONFIG.REDIS_PORT,
+    db: 0,
+  }),
+}));
+
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
+}));
+app.use(json());
+app.use(logger());
 
 app.use(views(path.join(
     __dirname,
@@ -26,23 +48,23 @@ app.use(views(path.join(
   ), {
     extension: 'pug'
   }
-))
+));
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 
 // routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  console.error('server error', err, ctx);
 });
 
-module.exports = app
+module.exports = app;
